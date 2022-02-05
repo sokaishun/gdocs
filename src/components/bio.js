1cdbe7ca272aa651917992f68f7a0273af180878
sokaishun <98691390+sokaishun@users.noreply.github.com>
2022-02-05 23:38:07 +0900

    Update bio.js
    delete written by

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,7 +44,7 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+           {author?.summary || null}
           {` `}
         </p>
       )}
