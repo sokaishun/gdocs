b803bc9e31080408d33d3e57c933b98ac891f78f
sokaishun <98691390+sokaishun@users.noreply.github.com>
2022-02-12 22:04:16 +0900

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -33,7 +33,7 @@ const Bio = () => {
   return (
     <div className="bio">
       <StaticImage
-        className="inline object-cover w-16 h-16 mr-2 rounded-full"
+        className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
         src="../images/profile-pic.png"
