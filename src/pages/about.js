f5b3c7db3f263d9b9fe36b015a311e60d93950e8
sokaishun <98691390+sokaishun@users.noreply.github.com>
2022-02-19 00:10:13 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,11 +14,27 @@ const Aboutpage = ({ data, location }) => {
         <SEO title="About" />
         <h2>自己紹介</h2>
         <ul>
-          <li>2010/3 大学院卒業</li>
-          <li>2010/4 製造業会社へ入社、以降設備の開発を従事</li>
+          <li>2010/3 大学院卒業　研究内容:画像処理</li>
+          <li>2010/4 入社　仕事内容:主に自動検査設備の開発</li>
         </ul>
-        <h2>現在の仕事内容</h2>
-        <p>エンジニアとして働いている。主に設備の開発を中心。</p>
+
+       <h2>仕事で使っている技術</h2>
+         <ul>
+          <li>.net</li>
+          <li>Azure devops server</li>
+          <li>Mkdocs</li>
+          <li>Visual Studio/VSCode</li>
+          <li>git</li>
+          <li>Markdown</li>
+          <li>Ansible</li>
+        </ul>
+
+        <h2>興味ある技術</h2>
+         <ul>
+          <li>Devops</li>
+          <li>CMS</li>
+          <li>自動化</li>
+        </ul>
       </Layout>
     </div>
   );
