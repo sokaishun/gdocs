46b462fb39d1752168c1ae5d2b8448715399e03f
songhaijun <arschof@gmail.com>
2022-03-05 21:53:57 +0900

    fix about page layout bug

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <SiDotnet size={50} className="w-20 m-2 fill-blue-500"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2010年より</p>
               <p >設備使うパソコンのアプリケーションの開発</p>
             </div>
@@ -37,7 +37,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <SiAzuredevops size={50} className="w-20 m-2 fill-blue-500"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
               <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2013年より</p>
               <p >アプリケーションのソースコード管理、CIパイプライン</p>
             </div>
@@ -47,7 +47,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <div className="w-20 m-2 text-center">Mkdocs</div>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2020年より</p>
               <p >Markdown文書をウェブページ化</p>
             </div>
@@ -57,7 +57,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <SiVisualstudio size={50} className="w-20 m-2 fill-purple-600"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2020年より</p>
               <p >Visual Basicアプリケーション開発IDE、Markdown文書の作成用、またはJavascriptのコード作成</p>
             </div>
@@ -67,7 +67,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <FaGit size={50}  className="w-20 m-2"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2020年より</p>
               <p >ソースコードまたはMarkdown文書のバージョン管理</p>
             </div>
@@ -77,7 +77,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <FaMarkdown size={50} className="w-20 m-2"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2020年より</p>
               <p >ソフトウェアの仕様書作成用</p>
             </div>
@@ -87,7 +87,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <SiAnsible size={50} className="w-20 m-2 fill-stone-400"/>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2022年より</p>
               <p >パソコンのセットアップやソフトウェアの配布の自動化</p>
             </div>
@@ -97,7 +97,7 @@ const Aboutpage = ({ data, location }) => {
           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 
           dark:hover:bg-gray-700">
             <div className="w-20 m-2 text-center">Halcon</div>
-            <div className="flex flex-col justify-between pl-5">
+            <div className="flex flex-col justify-between pl-5 w-4/5">
              <p className="flex flex-row items-center mt-0 mb-0"><FaCalendarAlt className="mr-1"/>2010年より</p>
               <p >画像処理アプリケーション開発用のライブラリ</p>
             </div>
