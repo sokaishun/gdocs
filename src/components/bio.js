20b4d950cb7531217e8a2c72a385e7a96b8d7581
songhaijun <arschof@gmail.com>
2022-03-19 21:05:41 +0900

    delete flowbite

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,7 +9,6 @@ import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
-import "flowbite";
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
